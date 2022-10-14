cd3d51f0ee20858b07a400a71767aee42ba7728d
Aza <113343842+220879cs07@users.noreply.github.com>
2022-10-14 12:04:08 +0500

    feat: solve all 14 problems

--- loops.cpp
+++ loops.cpp
@@ -170,3 +170,105 @@ int main() {
     
     return 0;
 } */
+
+
+/* Problem 10
+int main() {
+    int number = 0;
+    double one = 1;
+    double sum = 0;
+    
+    cout << "Enter number: "; cin >> number;
+    
+    for (int i = 1; i <= number; i++) {
+        sum += (one / i);
+    }
+    
+    cout << "Result: " << sum << endl;
+    
+    return 0;
+} */
+
+
+/* Problem 11
+int main() {
+    int binary = 0;
+    
+    cout << "Enter binary: "; cin >> binary;
+    
+    int decimal = 0;
+    int power = 0;
+
+    while (binary != 0) {
+        int reminder = binary % 10;
+        binary = binary / 10;
+
+        decimal += reminder * pow(2, power);
+        power++;
+    }
+    
+    cout << "The decimal number is " << decimal << endl;
+    
+    return 0;
+} */
+
+
+/* Problem 12
+int main() {
+    int number = 0;
+    int sum = 0;
+    
+    cout << "Enter number: "; cin >> number;
+    
+    while (number != 0) {
+        int last_digit = number % 10;
+        number = number / 10;
+        
+        sum += last_digit;
+    }
+    
+    cout << "The sum of digits is " << sum << endl;
+} */
+
+
+/* Problem 13
+int main() {
+    int count_subjects = 0;
+    float total = 0;
+    
+    cout << "Enter count of subjects: "; cin >> count_subjects;
+        
+    for (int i = 1; i <= count_subjects; i++) {
+        float value = 0;
+        
+        cout << i << ") "; cin >> value;
+        
+        total += value;
+    }
+    
+    cout << "Your total GPA is " << (total / count_subjects) << endl;
+    
+    return 0;
+} */
+
+
+/* Problem 14
+int main() {
+    int number = 0;
+    
+    cout << "Enter number: "; cin >> number;
+    
+    if (number != 0) {
+        for (int i = 1; i <= number; i++) {
+            string value = "";
+            
+            for (int j = 0; j < i; j++) {
+                value += to_string(i);
+            }
+            
+            cout << value << endl;
+        }
+    } else {
+        cout << "The depth is 0" << endl;
+    }
+} */

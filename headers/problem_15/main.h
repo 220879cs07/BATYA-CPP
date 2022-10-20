faa965a7baaf66b8074393c2b47a78d32a003c9f
Aza <113343842+220879cs07@users.noreply.github.com>
2022-10-20 21:55:57 +0500

    feat: problem_15

---
+++ headers/problem_15/main.h
@@ -0,0 +1,13 @@
+#ifndef AKFA_MAIN_H
+#define AKFA_MAIN_H
+
+#include <iostream>
+using namespace std;
+
+void print(float a, float b);
+float add(float a, float b);
+float subtract(float a, float b);
+float product(float a, float b);
+float division(float a, float b);
+
+#endif

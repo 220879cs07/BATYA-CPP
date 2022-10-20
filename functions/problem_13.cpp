55a3c221670cc926e31da832f194c43bca6e92d6
Aza <113343842+220879cs07@users.noreply.github.com>
2022-10-20 21:30:12 +0500

    feat: problem_13

---
+++ functions/problem_13.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double arr[7] = {3.3, 4.55, 1.111, 44.55, 66.66, 34.234234, 12.3123213};
+    cout << arr[4] << endl;
+
+    return 0;
+}
+

fc9915791d37119aee1c636bfc0fc50005871c9e
Aza <113343842+220879cs07@users.noreply.github.com>
2022-10-20 21:56:17 +0500

    feat: problem_14

---
+++ headers/problem_15/utils.cpp
@@ -0,0 +1,17 @@
+#include "main.h"
+
+float add(float a, float b) {
+    return a + b;
+}
+
+float subtract(float a, float b) {
+    return a - b;
+}
+
+float product(float a, float b) {
+    return a * b;
+}
+
+float division(float a, float b) {
+    return a / b;
+}

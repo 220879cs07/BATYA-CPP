a1f452f27636174b3e9d69ea07219da2b34c4268
Aza <113343842+220879cs07@users.noreply.github.com>
2022-10-26 10:21:29 +0500

    feat: odd or even

---
+++ midterm-practice/odd-even.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+    
+    cout << "Enter number: ";
+    cin >> number;
+    
+    for (int i = 0; i < number; i++) {
+        if (i % 2 == 0) {
+            cout << i << " ";
+        }
+    }
+    
+    return 0;
+}
